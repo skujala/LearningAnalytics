d4b452549b7b4c0ddccfda33b47207fe41f890d0
Sami Kujala <skujala@iki.fi>
2017-08-11 15:23:08 +0300

    Data import and normalization works

--- Koosta_kurssisuoritukset.R
+++ Koosta_kurssisuoritukset.R
@@ -8,114 +8,98 @@ require(ggthemes)
 require(RColorBrewer)
 
 tulokset.meta = list(
-  "2013"="../2013/Mekaniikka 2013 tulokset.xlsx",
-  "2014_1"="../2014/Arvostelu_20150105.xlsx",
-  "2014_2"="../2014/Arvostelu_20150105_erilliset.xlsx",
-  "2015"="../2015/Valikokeet/ELEC-A3110-arvosanat.xlsx",
-  "2016"="../2016/ELEC-A3110_2016_tulokset.xlsx"
+  "2013"   = list(
+    file="../2013/Mekaniikka 2013 tulokset_analysis.xlsx", 
+    scales.et = rep(24, 10), scales.lh = rep(5,10), scale.matlab = 5,
+    colIndex = c(1:27, 36:39, 48)
+  ),
+  "2014"   = list(
+    file="../2014/Arvostelu_20150105.xlsx", 
+    scales.et = rep(c(4,2), 5), scales.lh = c(12, rep(c(3,6),4), 3), scale.matlab = 15,
+    colIndex = c(1:29, 36:38, 40)
+  ),
+  "2015"   = list(
+    file="../2015/Valikokeet/Arvostelu.xlsx",
+    scales.et = rep(c(4,2), 5), scales.lh = rep(4, 10), scale.matlab = 15,
+    colIndex = c(1:30, 55:57, 59, 63)
+  ),
+  "2016"   = list(
+    file="../2016/ELEC-A3110_2016_tulokset.xlsx",
+    scales.et = rep(3, 10), scales.lh = rep(4, 10), scale.matlab = 21,
+    colIndex = c(1:29, 37:39, 41, 44)
+  )
 )
 
-
-
-
-# # lue välikokeiden tulokset kaikki kerralla yhteen dataframeen
-# palautteet.data <- do.call(
-#   rbind,
-#   c(
-#     llply(
-#       palautteet.meta, 
-#       function(x) { 
-#         tmp <- read.xlsx(
-#           x$File, 
-#           sheetIndex = 1, 
-#           colIndex = which(complete.cases(x$Columns)) # Only import columns we need -- homogenize data
-#         ); 
-#         colnames(tmp) <- na.omit(x$Columns);          # Rename those columns we choose to use 
-#         tmp[tmp == '0'] <- NA;                        # Stupid f*ing Webropol signifies NA with zeros...replace them with NA
-#         
-#         # Replace any remaining "Unable to respond" with NA's
-#         tmp[tmp == 'E'] <- NA;
-#         tmp[tmp == "Ei perusteita vastata"] <- NA;
-#         
-#         # Convert response time to POSIXct timestamp
-#         tmp$Vastausaika <- strptime(tmp$Vastausaika, "%d.%m.%Y %H:%M:%S")
-#         
-#         # Homogenize factors 
-#         levels(tmp$Yleisarvio) <- grade_factors
-#         levels(tmp$Toteutustapa) <- likert_factors
-#         levels(tmp$Oma_panos) <- likert_factors
-#         levels(tmp$Ymmarrys) <- likert_factors
-#         levels(tmp$Arvostelun_linjakkuus) <- likert_factors
-#         
-#         levels(tmp$Tyoaika) <- work_likert_factors
-#         levels(tmp$Lasnaolo) <- presence_factors
-#         
-#         # Add column for year
-#         tmp$Vuosi <- tmp$Vastausaika$year+1900
-#         
-#         for (col in cols_to_fix) {
-#           tmp[,col] <- ordered(
-#             tmp[,col], 
-#             levels=c(
-#               "Täysin eri mieltä",
-#               "Jonkin verran eri mieltä",
-#               "Ei samaa eikä eri mieltä",
-#               "Jonkin verran samaa mieltä",
-#               "Täysin samaa mieltä"
-#               #"Ei perusteita vastata"
-#             ) 
-#           )
-#           
-#         }
-#         
-#         return(tmp) 
-#       }    
-#     ),
-#     make.row.names=F
-#   )
-# )
-
-
-melt_data <- function(c, x) {
+fix_colnames <- function(x) {
+  x <- gsub("ID.number", "Opnro", x)
+  x <- gsub("First.name", "Etunimi", x)
+  x <- gsub("Surname", "Sukunimi",x)
+  x <- gsub("Email.address", "Email", x)
+  x <- gsub("email", "Email", x)
+  x <- gsub("LH0", "LH", x)
+  x <- gsub(".max", "", x)
+  x <- gsub(".sum", ".total", x)
+  x <- gsub(".korjattu", "", x)
+  x <- gsub("^Matlab$", "Matlab.1", x)
+  x <- gsub("Matlab([12])", "Matlab.\\1", x)
+  x <- gsub("kurssipalaute", "Palautepiste", x)
+  x <- gsub("palautepiste", "Palautepiste", x)
+  x <- gsub("AS", "Arvosana", x)
   
-  x.table <- table(x[,'Vuosi'], x[,c])
-  x.percentage <- x.table / rowSums(x.table)
-  
-  x.melt <- merge(x.percentage, melt(x.table), by=c("Var1", "Var2"))
-  colnames(x.melt) <- c("Vuosi", "Vastaus", "Prosenttia", "N")
-  
-  return(x.melt)
+  return(x)
 }
 
-melt_data_likert <- function(c, x) {
+differences_fcn <- function(column) {
+  return (
+    function(x) {
+      d <- vapply(
+        seq(2,10),
+        function(idx) {
+          x[,paste(column, idx, sep="")] - x[,paste(column, idx-1, sep="")]
+        },
+        FUN.VALUE = numeric(nrow(x))
+      )
+      d <- cbind(x[,paste(column, 1, sep="")], d)
+      
+      colnames(d) <- paste(paste(column, ".d", sep=""), 0:9, sep="")
+    
+      return(d)
+    }
+  )
+}
+
+lh_diff <- differences_fcn("LH")
+et_diff <- differences_fcn("ET")
+
+tulokset = lapply(
+  tulokset.meta, function (x) {
+    d <- read.xlsx(x$file, sheetIndex = 1, colIndex = x$colIndex)
+    colnames(d) <- fix_colnames(colnames(d))
   
-  transform_table_to_likert <- function (y) {
-    negative_factors <- names(likert_factors)[1:2]
-    neutral_factor <- names(likert_factors)[3]
+    d[is.na(d)] <- 0
     
-    offsets <- melt(-y[, negative_factors[1]] - y[, negative_factors[2]] - 0.5 * y[,neutral_factor])
-    offsets$Year <- row.names(offsets)
+
+    d[,c(paste("ET", 1:10, sep=""), paste("LH", 1:10, sep=""), "Matlab.1")] <- scale(
+      d[,c(paste("ET", 1:10, sep=""), paste("LH", 1:10, sep=""), "Matlab.1")], 
+      center=F, 
+      scale=c(x$scales.et, x$scales.lh, x$scale.matlab)
+    )
     
+    d <- cbind(d, lh_diff(d))
+    d <- cbind(d, et_diff(d))
     
-    y <- melt(y)
-    y$start <- ave(y$value, by=y$Var1, FUN = cumsum)
-    y <- merge(y, offsets, by.x="Var1", by.y="Year", suffixes=c(".koko", ".offset"))
-    
-    y$Alku <- y$start + y$value.offset
-    y$start <- NULL
-    y$value.offset <- NULL
-    
-    colnames(y) <- c("Vuosi", "Vastaus", "Koko",  "Alku")
-    
-    return(y)
+    d$Vuosi <- unlist(strsplit(x$file, "/"))[2]
+    d$Lapi <- "Ei"
+    d$Lapi[d$Arvosana != 0] <- "Kyllä"
+        
+    return(d)
   }
-  
-  
-  x.table <- table(x[,'Vuosi'], x[,c])
-  x.percentage <- transform_table_to_likert(x.table / rowSums(x.table))
-  
-  return(x.percentage)
-}
+)
+
+# find common columns and form one huge dataframe
+tulokset.all <- Reduce(rbind, lapply(tulokset, function(x) {x[,Reduce(intersect, lapply(tulokset, colnames))]}))
+tulokset.all$Vuosi <- factor(tulokset.all$Vuosi)
+tulokset.all$Lapi <- factor(tulokset.all$Lapi)
 
 # Now we have all the data in one huge data frame. Lovely. Start analyzing
 
