ba4c4e8e655b6fc031047d1a44989362b52f7b55
Sami Kujala <skujala@iki.fi>
2017-08-22 15:00:10 +0300

    Getting there!

--- Koosta_kurssisuoritukset.R
+++ Koosta_kurssisuoritukset.R
@@ -1,33 +1,33 @@
 #dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
 #require(xlsx)
 require(readxl)
-require(reshape2)
-require(plyr)
-require(knitr)
+require(dplyr)
+require(tidyr)
 require(ggplot2)
 require(ggthemes)
 require(RColorBrewer)
+require(sqldf)
 
 tulokset.meta = list(
   "2013"   = list(
     file="../2013/Mekaniikka 2013 tulokset_analysis.xlsx", 
     scales.et = rep(24, 10), scales.lh = rep(5,10), scale.matlab = 5,
-    colIndex = c(1:27, 36:39, 48)
+    range = c(1:30, 48) # Do not use midterm exam maximum points
   ),
   "2014"   = list(
     file="../2014/Arvostelu_20150105.xlsx", 
     scales.et = rep(c(4,2), 5), scales.lh = c(12, rep(c(3,6),4), 3), scale.matlab = 15,
-    colIndex = c(1:29, 36:38, 40)
+    range = c(1:32, 40) # Do not use midterm exam maximum points
   ),
   "2015"   = list(
     file="../2015/Valikokeet/Arvostelu.xlsx",
     scales.et = rep(c(4,2), 5), scales.lh = rep(4, 10), scale.matlab = 15,
-    colIndex = c(1:30, 55:57, 59, 63)
+    range = c(1:30, 34, 38, 42, 59, 63) # Do not use midterm exam maximum points
   ),
   "2016"   = list(
     file="../2016/ELEC-A3110_2016_tulokset.xlsx",
     scales.et = rep(3, 10), scales.lh = rep(4, 10), scale.matlab = 21,
-    colIndex = c(1:29, 37:39, 41, 44)
+    range = c(1:33, 41, 44)
   )
 )
 
@@ -74,7 +74,7 @@ et_diff <- differences_fcn("ET")
 
 tulokset = lapply(
   tulokset.meta, function (x) {
-    d <- read_excel(x$file, sheet = 1, range = cell_cols(x$colIndex))
+    d <- read_excel(x$file, sheet = 1, range = cell_cols(x$range))
     colnames(d) <- fix_colnames(colnames(d))
   
     d[!is.na(d$VK1) & d$VK1 == 0,"VK1"] <- NA
@@ -117,30 +117,63 @@ as.numeric(as.character(tulokset.all$Arvosana))
 
 # anonymize the data. Only interested in numerical data + grade (a factor)
 
-cols <- c(unlist(lapply(1:10, function(x) {paste0(c("ET", "LH"), x)})), "VK1", "VK2", "VK3", "Arvosana")
+cols <- c(
+  unlist(
+    lapply(
+      1:10,
+      function(x) {paste0(c("ET", "LH"), x)}
+      )
+    ),
+  "VK1", "VK2", "VK3", "Arvosana", "Vuosi"
+)
 
 analysis <- tulokset.all[,c(sapply(tulokset.all, function(x) {is.numeric(x) | is.factor(x)}))]
 analysis <- analysis[,cols]
 
-
-
-ahkeruus <- do.call(
-  rbind,
-  lapply(
+analysis <- cbind(
+  analysis, 
+  sapply(
     seq(2,20,by=2), 
     function(x) {
-      d <- as.data.frame(
-        table(
-          Läpi=analysis$Arvosana != 0, 
-          Tehtyjä=cut(rowSums(analysis[,seq(1,x)], na.rm=T), breaks=c(0,0.3,Inf)*x, right=F, labels=c("0-35%", "35-100%")),
-          Oli_välikokeessa=!is.na(analysis$VK1)
-        )
+      cut(
+        rowSums(
+          analysis[,seq(1,x)], na.rm=T
+        ), 
+        breaks=c(0,0.3,0.6,Inf)*x, right=F, 
+        labels=c("0-33%", "33-66%", "66-100%")
       )
-      d <- cbind(d, Viikko=rep(x/2, nrow(d)))
-      d
     }
   )
 )
+
+colnames(analysis)[26:35] <- sprintf("%02d", 1:10)
+
+analysis2 <- 
+  analysis[, c("Arvosana", "VK1", sprintf("%02d", 1:10))] %>%
+    gather(Viikko, Tehtyjä_tehtäviä, -Arvosana, -VK1)
+
+lapaisyt <- sqldf('
+  SELECT 
+    Viikko, 
+    "Tehtyjä_tehtäviä", 
+    case VK when 1 then "Kyllä" else "Ei" end as "Osallistui_VK1", 
+    count_true as "Läpäisseitä", 
+    count_true / count_all * 100 as "Läpäisyprosentti"
+  FROM (
+    SELECT 
+      cast(count(case "arvosana" when 0 then null else 1 end) as real) as count_true,
+      cast(count(*) as real) as count_all, 
+      "Tehtyjä_tehtäviä", 
+      Viikko, 
+      VK1,
+      case ifnull(VK1, 0) when 0 then 0 else 1 end as VK
+    FROM analysis2 
+    GROUP BY 
+      "Tehtyjä_tehtäviä",
+      Viikko,
+      VK
+  )
+')
 
 
 # Now we have all the data in one huge data frame. Lovely. Start analyzing
