505843473ed592e837f18f346a8bd8404da9b6e0
Sami Kujala <skujala@iki.fi>
2017-10-13 14:27:49 +0300

    Data massaging has now been dplyr'ized.
    Yay! I apparently have learned something from the DataCamp.

--- Koosta_kurssisuoritukset.R
+++ Koosta_kurssisuoritukset.R
@@ -1,13 +1,8 @@
-#dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
-#require(xlsx)
 require(readxl)
-require(dplyr)
-require(tidyr)
-require(ggplot2)
-require(ggthemes)
-require(RColorBrewer)
-require(sqldf)
+require(tidyverse)
 
+
+# List containing all the necessary metadata of the files
 tulokset.meta = list(
   "2013"   = list(
     file="../2013/Mekaniikka 2013 tulokset_analysis.xlsx", 
@@ -31,150 +26,68 @@ tulokset.meta = list(
   )
 )
 
-fix_colnames <- function(x) {
-  x <- gsub("ID.number", "Opnro", x)
-  x <- gsub("First.name", "Etunimi", x)
-  x <- gsub("Surname", "Sukunimi",x)
-  x <- gsub("Email.address", "Email", x)
-  x <- gsub("email", "Email", x)
-  x <- gsub("LH0", "LH", x)
-  x <- gsub(".max", "", x)
-  x <- gsub(".sum", ".total", x)
-  x <- gsub(".korjattu", "", x)
-  x <- gsub("^Matlab$", "Matlab.1", x)
-  x <- gsub("Matlab([12])", "Matlab.\\1", x)
-  x <- gsub("kurssipalaute", "Palautepiste", x)
-  x <- gsub("palautepiste", "Palautepiste", x)
-  x <- gsub("AS", "Arvosana", x)
-  
-  return(x)
-}
-
-differences_fcn <- function(column) {
-  return (
-    function(x) {
-      d <- sapply(
-        seq(2,10),
-        function(idx) {
-          x[,paste(column, idx, sep="")] - x[,paste(column, idx-1, sep="")]
-        }#,
-        #FUN.VALUE = numeric(nrow(x))
-      )
-      d <- cbind(x[,paste(column, 1, sep="")], d)
-      
-      colnames(d) <- paste(paste(column, ".d", sep=""), 0:9, sep="")
+# Read all of the files at once, preprocess the data and massage in into a list of tibbles
+tulokset = map(
+  tulokset.meta, 
+  function (x) {
+    scale <- c(x$scales.et, x$scales.lh, x$scale.matlab)
     
-      return(d)
-    }
-  )
-}
-
-lh_diff <- differences_fcn("LH")
-et_diff <- differences_fcn("ET")
-
-tulokset = lapply(
-  tulokset.meta, function (x) {
-    d <- read_excel(x$file, sheet = 1, range = cell_cols(x$range))
-    colnames(d) <- fix_colnames(colnames(d))
-  
-    d[!is.na(d$VK1) & d$VK1 == 0,"VK1"] <- NA
-    d[!is.na(d$VK2) & d$VK2 == 0,"VK2"] <- NA
-    d[!is.na(d$VK3) & d$VK3 == 0,"VK3"] <- NA
-    
-    #d[is.na(d)] <- 0
-
-    d[,c(paste("ET", 1:10, sep=""), paste("LH", 1:10, sep=""), "Matlab.1")] <- scale(
-      d[,c(paste("ET", 1:10, sep=""), paste("LH", 1:10, sep=""), "Matlab.1")], 
-      center=F, 
-      scale=c(x$scales.et, x$scales.lh, x$scale.matlab)
-    )
-    
-    #d <- cbind(d, lh_diff(d))
-    #d <- cbind(d, et_diff(d))
-    
-    d$Vuosi <- as.numeric(unlist(strsplit(x$file, "/"))[2])
-
-    return(d)
+    read_excel(x$file, sheet = 1, range = cell_cols(x$range)) %>%
+      # Unify column names. Stupid me for not having done this years ago when creating the files
+      # Attempt to follow the principles of reproducible research, so make no changes into the files
+      # themselves!
+      rename_all(
+        funs(
+          stringr::str_replace_all(., " ", "_") %>%
+          tolower(.) %>%
+          # These probably should be refactored into a map2 call for easier maintenance
+          stringr::str_replace_all(., "id.number", "opnro") %>%
+          stringr::str_replace_all(., "first.name", "etunimi") %>%
+          stringr::str_replace_all(., "surname", "sukunimi") %>%
+          stringr::str_replace_all(., "email.address", "email") %>%
+          stringr::str_replace_all(., "lh0", "lh") %>%
+          stringr::str_replace_all(., ".yht", "") %>%
+          stringr::str_replace_all(., ".sum", ".totals") %>%
+          stringr::str_replace_all(., "^matlab$", "matlab.1") %>%
+          stringr::str_replace_all(., "matlab([12])", "matlab.\\1") %>%
+          stringr::str_replace_all(., "kurssipalaute", "palautepiste") %>%
+          stringr::str_replace_all(., "as", "arvosana")
+        )
+      ) %>%
+      # Convert midterm results with zero points to NA's to signify student not being present
+      mutate(vk1 = ifelse(vk1 == 0, NA, vk1),
+             vk2 = ifelse(vk2 == 0, NA, vk2),
+             vk3 = ifelse(vk3 == 0, NA, vk3)) %>%
+      # Ugly(-ish) hack! 
+      #
+      # Nest the numeric data for easier scaling. A bit slow...involves creating many lists. 
+      #
+      # This would benefit from a refactor, but I do not know how to do
+      # scale the data using dplyr pipelines, when the scaling depends on the variable being scaled. 
+      nest(et1:et10, lh1:lh10, matlab.1) %>% 
+        # Scale the scores to range 0-1, as the score ranges have changed over the years
+        # This would be perfect spot to perform other computations, like fitting models 
+        # or such-like
+        mutate(data=map(data, ~ . / scale)) %>% 
+      # Un-nest when we are done
+      unnest(data) %>% 
+      # Add column for the year. This is obtained from the file path. WILL BREAK if the files are fetched 
+      # from somewhere else
+      mutate(vuosi = as.numeric(unlist(strsplit(x$file, "/"))[2]))
   }
 )
 
-
-# find common columns and form one huge dataframe
-tulokset.all <- Reduce(rbind, lapply(tulokset, function(x) {x[,Reduce(intersect, lapply(tulokset, colnames))]}))
-tulokset.all$Arvosana <- factor(tulokset.all$Arvosana)
-
-
-# Compute activity ("did student attempt Quiz or Exercise" -- 0, 1, 2)
-aktiivisuus <- cbind(sapply(
-  1:10, 
-  function(x, y) {
-    rowSums(!is.na(y[,paste0(c("ET", "LH"), x)]))
-  },
-  tulokset.all
-),
-as.numeric(as.character(tulokset.all$Arvosana))
-)
-
-# anonymize the data. Only interested in numerical data + grade (a factor)
-
-cols <- c(
-  unlist(
-    lapply(
-      1:10,
-      function(x) {paste0(c("ET", "LH"), x)}
-      )
-    ),
-  "VK1", "VK2", "VK3", "Arvosana", "Vuosi"
-)
-
-analysis <- tulokset.all[,c(sapply(tulokset.all, function(x) {is.numeric(x) | is.factor(x)}))]
-analysis <- analysis[,cols]
-
-analysis <- cbind(
-  analysis, 
-  sapply(
-    seq(2,20,by=2), 
+# Find common columns and form one data frame for all data 
+tulokset.all <- bind_rows(
+  map(
+    tulokset, 
     function(x) {
-      cut(
-        rowSums(
-          analysis[,seq(1,x)], na.rm=T
-        ), 
-        breaks=c(0,0.3,0.6,Inf)*x, right=F, 
-        labels=c("0-33%", "33-66%", "66-100%")
-      )
+      x %>% 
+        select(!!Reduce(intersect, map(tulokset, colnames)))
     }
   )
 )
 
-colnames(analysis)[26:35] <- sprintf("%02d", 1:10)
-
-analysis2 <- 
-  analysis[, c("Arvosana", "VK1", sprintf("%02d", 1:10))] %>%
-    gather(Viikko, Tehtyjä_tehtäviä, -Arvosana, -VK1)
-
-lapaisyt <- sqldf('
-  SELECT 
-    Viikko, 
-    "Tehtyjä_tehtäviä", 
-    case VK when 1 then "Kyllä" else "Ei" end as "Osallistui_VK1", 
-    count_true as "Läpäisseitä", 
-    count_true / count_all * 100 as "Läpäisyprosentti"
-  FROM (
-    SELECT 
-      cast(count(case "arvosana" when 0 then null else 1 end) as real) as count_true,
-      cast(count(*) as real) as count_all, 
-      "Tehtyjä_tehtäviä", 
-      Viikko, 
-      VK1,
-      case ifnull(VK1, 0) when 0 then 0 else 1 end as VK
-    FROM analysis2 
-    GROUP BY 
-      "Tehtyjä_tehtäviä",
-      Viikko,
-      VK
-  )
-')
-
-
-# Now we have all the data in one huge data frame. Lovely. Start analyzing
+# Now we have all the data in one huge data frame. Lovely. Start analyzing. 
+# Remember that the data still students' names so it's not anonymous
 
